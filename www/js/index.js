e8377f5d29d63d734a88ebbf9210cdd1951f65e3
laercio Simoes <laercio@hpcbrasil.com>
2014-02-02 12:48:12 -0300

    Update Icon Images

--- www/js/index.js
+++ www/js/index.js
@@ -47,3 +47,59 @@ var app = {
         console.log('Received Event: ' + id);
     }
 };
+
+$(document).ready(function () {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            var lat = position.coords.latitude;
+            var long = position.coords.longitude;
+            position.coords
+            console.log('Your latitude is :' + lat + ' and longitude is ' + long);
+            var coord = lat.toString() + '|' + long.toString();
+            $.get("api/location", { id: coord }, function (data) {
+                $("#txtDistrict").val(data);
+            });
+        }, function error(err) {
+            console.warn('ERROR(' + err.code + '): ' + err.message);
+        });
+    }
+
+
+    $("#btnSearch").click(function () {
+        console.log("Click Search");
+        if ($("#cmbSpecialty").val() == "0" || $("#cmbSpecialty").val() == null) {
+            alert("Necessario selecionar a Especialidade");
+            return false;
+        }
+        if ($("#cmbHealthInsurance").val() == null) {
+            alert("Necessario Selecionar o Tipo de Convenio");
+            return false;
+        }
+        $("#form1").submit();
+    });
+
+
+    var specialty = $("#cmbSpecialty");
+    var url = "http://velozmed.azurewebsites.net/api/Specialties";
+
+    $.getJSON(url, function (data) {
+        specialty.html('');
+        for (i = 0; i < data.length; i++) {
+            specialty.append('<option value="' + data[i].Id + '">' + data[i].Name + '</option>');
+        }
+        specialty.append('</select>');
+    });
+
+
+
+    var healthInsurance = $("#cmbHealthInsurance");
+    var url = "http://velozmed.azurewebsites.net/api/HealthInsurances";
+
+    $.getJSON(url, function (data) {
+        healthInsurance.html('');
+        for (i = 0; i < data.length; i++) {
+            healthInsurance.append('<option value="' + data[i].Id + '">' + data[i].Name + '</option>');
+        }
+        healthInsurance.append('</select>');
+    })
+});

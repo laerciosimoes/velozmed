301f25aadf67957b652747a165b7363650457332
laercio Simoes <laercio@hpcbrasil.com>
2014-02-02 00:56:07 -0300

    Home Page

---
+++ platforms/android/gen/R.java.d
@@ -0,0 +1,9 @@
+C:\TFS2013\velozmed\Main\Velozmed\velozmed-mobile\platforms\android\gen\com\phonegap\helloworld\R.java \
+ : C:\TFS2013\velozmed\Main\Velozmed\velozmed-mobile\platforms\android\res\drawable\icon.png \
+C:\TFS2013\velozmed\Main\Velozmed\velozmed-mobile\platforms\android\res\drawable-hdpi\icon.png \
+C:\TFS2013\velozmed\Main\Velozmed\velozmed-mobile\platforms\android\res\drawable-ldpi\icon.png \
+C:\TFS2013\velozmed\Main\Velozmed\velozmed-mobile\platforms\android\res\drawable-mdpi\icon.png \
+C:\TFS2013\velozmed\Main\Velozmed\velozmed-mobile\platforms\android\res\drawable-xhdpi\icon.png \
+C:\TFS2013\velozmed\Main\Velozmed\velozmed-mobile\platforms\android\res\values\strings.xml \
+C:\TFS2013\velozmed\Main\Velozmed\velozmed-mobile\platforms\android\res\xml\config.xml \
+C:\TFS2013\velozmed\Main\Velozmed\velozmed-mobile\platforms\android\bin\AndroidManifest.xml \
